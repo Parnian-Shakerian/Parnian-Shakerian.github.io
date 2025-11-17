b01770f2330303cc56ecc5177b143a65c94cf60b
Parnian Shakerian <86061662+Parnian-Shakerian@users.noreply.github.com>
2025-11-18 01:04:30 +0330

    Update scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -1,24 +1,18 @@
 /*!
-* Start Bootstrap - Resume v7.0.5 (https://startbootstrap.com/theme/resume)
-* Copyright 2013-2022 Start Bootstrap
-* Licensed under MIT (https://github.com/StartBootstrap/startbootstrap-resume/blob/master/LICENSE)
+* Start Bootstrap - Resume v7.0.5 ...
 */
-//
-// Scripts
-// 
 
+// Scripts
 window.addEventListener('DOMContentLoaded', event => {
 
-    // Activate Bootstrap scrollspy on the main nav element
     const sideNav = document.body.querySelector('#sideNav');
     if (sideNav) {
         new bootstrap.ScrollSpy(document.body, {
             target: '#sideNav',
             offset: 74,
         });
-    };
+    }
 
-    // Collapse responsive navbar when toggler is visible
     const navbarToggler = document.body.querySelector('.navbar-toggler');
     const responsiveNavItems = [].slice.call(
         document.querySelectorAll('#navbarResponsive .nav-link')
@@ -35,10 +29,25 @@ window.addEventListener('DOMContentLoaded', event => {
 
 // not using jquery
 window.onload = function(){
-  var anchors = document.getElementsByTagName('a');
-  for (var i=0; i<anchors.length; i++){
-	if (!anchors[i].getAttribute('class') || anchors[i].getAttribute('class') === 'social-icon')
-      anchors[i].setAttribute('target', '_blank');
-  }
+    var anchors = document.getElementsByTagName('a');
+    for (var i=0; i<anchors.length; i++){
+        if (!anchors[i].getAttribute('class') || anchors[i].getAttribute('class') === 'social-icon')
+        anchors[i].setAttribute('target', '_blank');
+    }
 }
+
 // jquery is prettier. :-)
+
+
+document.querySelectorAll('.certificates img').forEach(img => {
+    img.addEventListener('click', () => {
+        if (img.classList.contains('zoomed')) {
+            img.classList.remove('zoomed');
+            document.body.classList.remove('no-scroll');
+        } else {
+            document.querySelectorAll('.certificates img').forEach(i => i.classList.remove('zoomed'));
+            img.classList.add('zoomed');
+            document.body.classList.add('no-scroll');
+        }
+    });
+});
